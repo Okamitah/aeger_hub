dab8d3dcf6cb5be99be863c69bb3fd106a9b09a2
Hatiim <179641048+Okamitah@users.noreply.github.com>
2025-12-12 15:15:59 +0100

    Some additional changes

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import { onMounted } from 'vue'
 
 onMounted(async () => {
@@ -18,15 +17,10 @@ onMounted(async () => {
 
 <template>
   <header>
-
     <div class="wrapper">
       <HelloWorld msg="Hello World!" />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
@@ -39,10 +33,6 @@ header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
